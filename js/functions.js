41c2bceb818e10de43525c8d1a0c4ff5b64ad16c
RaTeNo <rateno@mail.ru>
2021-05-31 13:44:05 +0300

    ddd

--- js/functions.js
+++ js/functions.js
@@ -28,7 +28,7 @@ $(() => {
 
 
 	// Маска ввода
-	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+	/*$('input[type=tel]').inputmask('+7 (999) 999-99-99')*/
 
 	// Кастомный select
 	$('select').niceSelect()
